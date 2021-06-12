a8ecad9a8f4ce4d35a5e7784d5072b1ffcb89bf2
vladynson <vladynson@yahoo.com>
2021-06-12 16:55:18 +0300

    test

--- CatalogDeNote/Form1.Designer.cs
+++ CatalogDeNote/Form1.Designer.cs
@@ -29,11 +29,22 @@ namespace CatalogDeNote
         /// </summary>
         private void InitializeComponent()
         {
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(298, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(906, 470);
+            this.Controls.Add(this.button2);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -43,6 +54,7 @@ namespace CatalogDeNote
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
