1f8145e65b7f0a4769d82fce353cee386e20614e
vladynson <vladynson@yahoo.com>
2021-06-12 16:37:05 +0300

    Revert "Baza"
    This reverts commit 512ff43c36f85306575ef741bfa8cf59de78aff9.

--- CatalogDeNote/Form1.cs
+++ CatalogDeNote/Form1.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace CatalogDeNote
 {
-    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
