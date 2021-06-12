42da9c6251756430f7c2bc6e5ccc8408ea940bf5
vladynson <vladynson@yahoo.com>
2021-06-12 17:33:08 +0300

    Test

--- CatalogDeNote/Form1.cs
+++ CatalogDeNote/Form1.cs
@@ -31,5 +31,20 @@ namespace CatalogDeNote
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
