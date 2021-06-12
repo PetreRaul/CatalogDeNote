d95f360ce29ff36504c53f45cccb4d342ff0974d
vladynson <vladynson@yahoo.com>
2021-06-12 17:35:26 +0300

    Test2

--- CatalogDeNote/Form1.Designer.cs
+++ CatalogDeNote/Form1.Designer.cs
@@ -32,7 +32,7 @@ namespace CatalogDeNote
             this.disciplinebtn = new System.Windows.Forms.Button();
             this.studentibtn = new System.Windows.Forms.Button();
             this.catalogbtn = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btniesire = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // disciplinebtn
@@ -80,25 +80,25 @@ namespace CatalogDeNote
             this.catalogbtn.UseVisualStyleBackColor = false;
             this.catalogbtn.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button2
+            // btniesire
             // 
-            this.button2.BackColor = System.Drawing.Color.Teal;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Location = new System.Drawing.Point(110, 337);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(191, 40);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "IESIRE";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.btniesire.BackColor = System.Drawing.Color.Teal;
+            this.btniesire.FlatAppearance.BorderSize = 0;
+            this.btniesire.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btniesire.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btniesire.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btniesire.Location = new System.Drawing.Point(110, 337);
+            this.btniesire.Name = "btniesire";
+            this.btniesire.Size = new System.Drawing.Size(191, 40);
+            this.btniesire.TabIndex = 4;
+            this.btniesire.Text = "IESIRE";
+            this.btniesire.UseVisualStyleBackColor = false;
+            this.btniesire.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(906, 470);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btniesire);
             this.Controls.Add(this.disciplinebtn);
             this.Controls.Add(this.studentibtn);
             this.Controls.Add(this.catalogbtn);
@@ -116,7 +116,7 @@ namespace CatalogDeNote
         private System.Windows.Forms.Button disciplinebtn;
         private System.Windows.Forms.Button studentibtn;
         private System.Windows.Forms.Button catalogbtn;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btniesire;
     }
 }
 
